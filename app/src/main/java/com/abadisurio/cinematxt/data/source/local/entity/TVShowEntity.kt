67ba9c4d8c42c821549d4585696b09843e8f100e
Abadi Suryo <abadisuryosetiyo@gmail.com>
2021-05-26 21:27:43 +0700

    Room: Refactor local entities

--- app/src/main/java/com/abadisurio/cinematxt/data/source/local/entity/TVShowEntity.kt
+++ app/src/main/java/com/abadisurio/cinematxt/data/source/local/entity/TVShowEntity.kt
@@ -1,9 +1,24 @@
 package com.abadisurio.cinematxt.data.source.local.entity
 
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tvshowentities")
 data class TVShowEntity(
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "id")
     var tvShowId: String,
+    @ColumnInfo(name = "title")
     var title: String,
+    @ColumnInfo(name = "description")
     var description: String,
+    @ColumnInfo(name = "releaseDate")
     var releaseDate: String,
-    var imagePath: String
+    @ColumnInfo(name = "imagePath")
+    var imagePath: String,
+    @ColumnInfo(name = "favorited")
+    var favorited: Boolean = false
 )
